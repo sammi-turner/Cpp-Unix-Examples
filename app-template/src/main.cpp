2932d57613be0e973a3658a01eb14b3f100c231c
Sammi Turner <ms.sammi.turner@gmail.com>
2023-05-19 23:33:53 +0100

    Add files via upload

---
+++ app-template/src/main.cpp
@@ -0,0 +1,10 @@
+#include "boilerplate.h"
+
+using namespace utils;
+
+int main()
+{
+    openVirtualTerminal();
+    vPuts("Hello, world!\n\n");
+    closeVirtualTerminal("PRESS ANY KEY TO CLOSE");
+}
