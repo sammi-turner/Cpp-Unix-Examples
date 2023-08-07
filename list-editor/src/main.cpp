08a033e8c403ef6dad642e8049533c93ceda145b
Sammi Turner <ms.sammi.turner@gmail.com>
2023-08-07 01:27:48 +0100

    Add files via upload

---
+++ list-editor/src/main.cpp
@@ -0,0 +1,15 @@
+#include "utils.h"
+#include "list-edit.h"
+
+using namespace utils;
+
+int main()
+{
+    openVirtualTerminal();
+    virtualCursorOff();
+
+    ListEdit instance;
+    instance.mainLoop();
+
+    closeVirtualTerminal("\n    PRESS ANY KEY TO CLOSE");
+}
