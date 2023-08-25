f4f91540df013d844fc8396589f16f9ef0d83fdd
Sammi Turner <ms.sammi.turner@gmail.com>
2023-08-25 23:49:07 +0100

    Add files via upload

--- utils/main.cpp
+++ utils/main.cpp
@@ -7,5 +7,7 @@ int main()
     openVirtualTerminal();
     string s = fileRead("test.txt");
     vPuts(s);
-    closeVirtualTerminal("\nPRESS ANY KEY TO CLOSE");
+    string v = shell("sqlite3 --version");
+    vPuts(v);
+    closeVirtualTerminal("\n\nPRESS ANY KEY TO CLOSE");
 }
