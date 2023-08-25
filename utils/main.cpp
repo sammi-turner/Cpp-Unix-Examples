8dff8b084d243f2e7a1d4a95152bec7cdc9bca29
Sammi Turner <ms.sammi.turner@gmail.com>
2023-08-25 23:49:59 +0100

    Update main.cpp

--- utils/main.cpp
+++ utils/main.cpp
@@ -7,7 +7,5 @@ int main()
     openVirtualTerminal();
     string s = fileRead("test.txt");
     vPuts(s);
-    string v = shell("sqlite3 --version");
-    vPuts(v);
     closeVirtualTerminal("\n\nPRESS ANY KEY TO CLOSE");
 }
