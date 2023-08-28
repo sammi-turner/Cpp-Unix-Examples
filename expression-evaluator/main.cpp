aa56c642781fc19b5d5a237c0318342b4d0b5524
Sammi Turner <ms.sammi.turner@gmail.com>
2023-08-28 23:51:59 +0100

    Add files via upload

--- expression-evaluator/main.cpp
+++ expression-evaluator/main.cpp
@@ -4,95 +4,127 @@
 #include <stack>
 #include <stdexcept>
 
-class Tokenizer {
+using std::cout;
+using std::string;
+using std::vector;
+
+class Tokenizer 
+{
 private:
-    std::string expression;
+    string expression;
     int index = 0;
 
 public:
-    Tokenizer(const std::string& expr) : expression(expr) {
+    Tokenizer(const string& expr) : expression(expr) 
+    {
         // Trim the expression
         size_t first = expression.find_first_not_of(' ');
         size_t last = expression.find_last_not_of(' ');
         expression = expression.substr(first, (last - first + 1));
     }
 
-    std::vector<std::string> tokenize() {
-        std::vector<std::string> tokens;
-        std::string number;
+    vector<string> tokenize() 
+    {
+        vector<string> tokens;
+        string number;
         int openBrackets = 0;
         int closeBrackets = 0;
 
-        while (index < expression.length()) {
+        while (index < expression.length()) 
+        {
             char c = expression[index];
-            if (isdigit(c) || c == '.') {
+            if (isdigit(c) || c == '.') 
+            {
                 number += c;
-            } else {
-                if (!number.empty()) {
+            } 
+            else 
+            {
+                if (!number.empty()) 
+                {
                     tokens.push_back(number);
                     number.clear();
                 }
-                if (c == '+' || c == '-' || c == '*' || c == '/' || c == '(' || c == ')') {
-                    tokens.push_back(std::string(1, c));
+                if (c == '+' || c == '-' || c == '*' || c == '/' || c == '(' || c == ')') 
+                {
+                    tokens.push_back(string(1, c));
                     if (c == '(') openBrackets++;
                     if (c == ')') closeBrackets++;
-                } else if (!isspace(c)) {
-                    throw std::runtime_error("Illegal token found: " + std::string(1, c));
+                } 
+                else if (!isspace(c)) 
+                {
+                    throw std::runtime_error("Illegal token found: " + string(1, c));
                 }
             }
             index++;
         }
-        if (!number.empty()) {
+        if (!number.empty()) 
+        {
             tokens.push_back(number);
         }
-        if (openBrackets != closeBrackets) {
+        if (openBrackets != closeBrackets) 
+        {
             throw std::runtime_error("Mismatched brackets found in the expression.");
         }
         return tokens;
     }
 };
 
-class Read {
+class Read 
+{
 public:
-    static std::string input() {
-        std::string input;
+    static string input() 
+    {
+        string input;
         std::getline(std::cin, input);
         return input;
     }
 };
 
-class Evaluator {
+class Evaluator 
+{
 public:
-    double evaluate(const std::vector<std::string>& tokens) {
+    double evaluate(const vector<string>& tokens) 
+    {
         std::stack<double> numbers;
         std::stack<char> operators;
 
-        for (const std::string& token : tokens) {
-            if (token == "(") {
+        for (const string& token : tokens) 
+        {
+            if (token == "(") 
+            {
                 operators.push(token[0]);
-            } else if (token == ")") {
-                while (operators.top() != '(') {
+            } 
+            else if (token == ")") 
+            {
+                while (operators.top() != '(') 
+                {
                     double b = numbers.top(); numbers.pop();
                     double a = numbers.top(); numbers.pop();
                     numbers.push(applyOperation(operators.top(), a, b));
                     operators.pop();
                 }
                 operators.pop();  // Remove the opening bracket
-            } else if (isOperator(token)) {
+            } 
+            else if (isOperator(token)) 
+            {
                 char op = token[0];
-                while (!operators.empty() && hasPrecedence(op, operators.top())) {
+                while (!operators.empty() && hasPrecedence(op, operators.top())) 
+                {
                     double b = numbers.top(); numbers.pop();
                     double a = numbers.top(); numbers.pop();
                     numbers.push(applyOperation(operators.top(), a, b));
                     operators.pop();
                 }
                 operators.push(op);
-            } else {
+            } 
+            else 
+            {
                 numbers.push(std::stod(token));
             }
         }
 
-        while (!operators.empty()) {
+        while (!operators.empty()) 
+        {
             double b = numbers.top(); numbers.pop();
             double a = numbers.top(); numbers.pop();
             numbers.push(applyOperation(operators.top(), a, b));
@@ -102,22 +134,28 @@ public:
     }
 
 private:
-    bool isOperator(const std::string& token) {
+    bool isOperator(const string& token) 
+    {
         return token == "+" || token == "-" || token == "*" || token == "/";
     }
 
-    bool hasPrecedence(char op1, char op2) {
-        if (op2 == '(' || op2 == ')') {
+    bool hasPrecedence(char op1, char op2) 
+    {
+        if (op2 == '(' || op2 == ')') 
+        {
             return false;
         }
-        if ((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-')) {
+        if ((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-')) 
+        {
             return false;
         }
         return true;
     }
 
-    double applyOperation(char op, double a, double b) {
-        switch (op) {
+    double applyOperation(char op, double a, double b) 
+    {
+        switch (op) 
+        {
             case '+':
                 return a + b;
             case '-':
@@ -125,7 +163,8 @@ private:
             case '*':
                 return a * b;
             case '/':
-                if (b == 0.0) {
+                if (b == 0.0) 
+                {
                     throw std::runtime_error("Cannot divide by zero.");
                 }
                 return a / b;
@@ -135,19 +174,23 @@ private:
     }
 };
 
-int main() {
-    std::cout << "Enter the arithmetic expression: ";
-    std::string expression = Read::input();
+int main() 
+{
+    cout << "Enter the arithmetic expression: ";
+    string expression = Read::input();
 
-    try {
+    try 
+    {
         Tokenizer tokenizer(expression);
         Evaluator evaluator;
 
-        std::vector<std::string> tokens = tokenizer.tokenize();
+        vector<string> tokens = tokenizer.tokenize();
         double result = evaluator.evaluate(tokens);
-        std::cout << "Result: " << result << std::endl;
-    } catch (const std::exception& e) {
-        std::cout << "Error: " << e.what() << std::endl;
+        cout << "Result: " << result << "\n";
+    } 
+    catch (const std::exception& e) 
+    {
+        cout << "Error: " << e.what() << "\n";
     }
 
     return 0;
