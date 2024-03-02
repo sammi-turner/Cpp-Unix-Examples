93a0ee4103103f985858360b7e6f572a46acac65
Sammi Turner <ms.sammi.turner@gmail.com>
2024-03-02 12:54:53 +0000

    Update main.cpp

--- template/src/main.cpp
+++ template/src/main.cpp
@@ -2,26 +2,7 @@
 
 using namespace utils;
 
-struct functionCall
-{
-    str name;
-    vector<string> arguments;
-};
-
-struct infix
-{
-    string name;
-    string lhs;
-    string rhs;
-};
-
-
-
 int main() 
 {
-    Tree t;
-    t.insert("0", 1); // Insert 1 as a leaf of the root
-    t.insert("0", 2); // Insert 2 as another leaf of the root
-    t.insert("1", 3); // Insert 3 as a leaf of node 1
-    t.printTree();
+    cout << "Hello, world!\n";
 }
